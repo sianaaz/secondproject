406f17017b69052226a73b8b3ff948a7314a3ee3
sianaaz <sianazyberii@gmail.com>
2024-07-19 11:02:40 +0200

    finished artist-home page

---
+++ index.js
@@ -0,0 +1,28 @@
+const selectedArtist = localStorage.getItem("selectedArtist");
+const itemsSold = document.getElementById("items-sold");
+const incomeTotal = document.getElementById("total-income");
+
+if (selectedArtist) {
+  const nameOfArtist = document.getElementById("nameOfArtist");
+  nameOfArtist.textContent = selectedArtist;
+
+  const items = JSON.parse(localStorage.getItem("items")) || [];
+
+  let soldItems = 0;
+  let totalIncome = 0;
+  
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    if (item.artist === selectedArtist) {
+      if (item.dateSold && item.priceSold) {
+        soldItems++;
+        totalIncome += item.priceSold;
+      }
+    }
+  }
+
+  const totalItems = items.filter((item) => item.artist === selectedArtist).length;
+
+  itemsSold.textContent = `${soldItems}`;
+  incomeTotal.textContent = `$${totalIncome}`;
+}
